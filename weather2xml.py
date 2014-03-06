00b4d2c5e23dad7df9ef111efc8d2fe563a70bfe
alejandronb <ale90nb@gmail.com>
2014-03-06 18:14:32 +0100

    Terminado a un nivel no óptimo: falla Granada y Málaga por la api

--- weather2xml.py
+++ weather2xml.py
@@ -13,26 +13,6 @@ f = open("plantilla.html","r")
 salida = open("salida.html","w")
 html = ''
 
-def cardinalidad(direccion):
-	"Funcion que transforma los grados en cardinalidades"
-	for degree in str(direccion):
-		if direccion >= 337.5 and direccion < 22.5:
-			return "N"
-		elif direccion >= 22.5 and direccion < 67.5:
-			return "NE"
-		elif direccion >= 67.5 and direccion < 112.5:
-			return "E"
-		elif direccion >= 112.5 and direccion < 157.5:
-			return "SE"
-		elif direccion >= 157.5 and direccion < 202.5:
-			return "S"
-		elif direccion >= 202.5 and direccion < 247.5:
-			return "SO"
-		elif direccion >= 247.5 and direccion < 292.5:
-			return "O"
-		elif direccion >= 292.5 and direccion < 337.5:
-			return "NO"
-
 
 for linea in f:
 	html += linea
@@ -44,39 +24,26 @@ viento_km = []
 direccion_viento = []
 
 
-
 url = 'http://api.openweathermap.org/data/2.5/weather'
-
-
 
 for ciudad in ciudades:
 	parametros = {'q':'%s,spain' % ciudad,'mode':'xml','units':'metric','lang':'es'}
 	peticion = requests.get(url,params=parametros)
 	raiz = etree.fromstring(peticion.text.encode("utf-8"))
-
 	city = raiz.find("city")
 	ncity = city.attrib["name"]
 	temperature = raiz.find("temperature")
-	tempmax = temperature.attrib["max"]
-	tempmin = temperature.attrib["min"]
+	tempmax = round(float(temperature.attrib["max"]),1)
+	tempmin = round(float(temperature.attrib["min"]),1)
 	viento_dir = raiz.find("wind/direction")
-	direccion = viento_dir.attrib["value"]
+	direccion = viento_dir.attrib["name"]
 	viento_vel = raiz.find("wind/speed")
 	velocidad = viento_vel.attrib["value"]
 	temp_max.append(tempmax)
 	temp_min.append(tempmin)
 	viento_km.append(velocidad)
-	direccion_viento.append(cardinalidad(direccion))
-	print peticion.text
-#	viento = 
-#	direccion = 
-#	maxima = round(tempmax - 273,1)
-#	minima = round(tempmin - 273,1)
-#	vientokm = round(viento*1.61)
-#	temp_max.append(maxima)
-#	temp_min.append(minima)
-#	viento_km.append(vientokm)
-#	direccion_viento.append(cardinalidad(direccion))
+	direccion_viento.append(direccion)
+
 
 
 template = Template(html)
